2a3247366f1788be5f55fa1ec1a59fcfb20420d2
KP64 <karamalsadeh@hotmail.com>
2023-04-22 20:16:08 +0200

    Rotating Cube

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
     clippy::complexity,
     clippy::perf,
     clippy::pedantic,
-    clippy::nursery,
+    clippy::nursery
 )]
 #![allow(
     clippy::needless_pass_by_value,
@@ -20,25 +20,24 @@ use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     log::{self, LogPlugin},
     prelude::*,
-    window::{self, Window, WindowResolution},
+    window::{Window, WindowResolution},
 };
+use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 const WINDOW_DIMENSIONS: (f32, f32) = (1280., 720.);
-/* const WINDOW_DIMENSIONS: (f32, f32) = (960., 540.); */
 const WINDOW_WIDTH: f32 = WINDOW_DIMENSIONS.0;
 const WINDOW_HEIGHT: f32 = WINDOW_DIMENSIONS.1;
 
 fn main() {
     App::new()
-        /* .insert_resource(ClearColor(BACKGROUND_COLOR)) */
+        .init_resource::<RotatorSpeed>()
         .add_plugins(
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                        title: "Break The Blocks (BTB)".into(),
-                        resizable: false,
+                        title: "RGB Cube".into(),
                         ..Default::default()
                     }),
                     ..Default::default()
@@ -46,75 +45,64 @@ fn main() {
                 .set(LogPlugin {
                     level: log::Level::DEBUG,
                     filter: "debug,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
-                })
-                .set(AssetPlugin {
-                    watch_for_changes: true,
-                    ..Default::default()
                 }),
         )
+        .add_plugin(NoCameraPlayerPlugin)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(WorldInspectorPlugin::new())
-        .add_startup_system(setup)
+        .add_plugin(WorldInspectorPlugin::default())
+        .add_startup_system(setup_environment)
         .add_startup_system(setup_rgb_cube)
-        .add_system(window::close_on_esc)
-        .add_system(rotate_camera)
+        .add_system(rotate_rgb_cube)
         .run();
 }
 
-fn rotate_camera(query: Query<(&mut Transform, &Name)>) {
-    for (transform, name) in query.iter() {
-        if name.contains("Route") {
-            continue;
-        }
+fn rotate_rgb_cube(
+    mut query: Query<&mut Transform, With<Rotator>>,
+    time: Res<Time>,
+    rot_speed: Res<RotatorSpeed>,
+) {
+    for mut transform in &mut query {
+        transform.rotate_y(rot_speed.0 * time.delta_seconds());
     }
 }
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
-    // Camera
+fn setup_environment(mut commands: Commands) {
+    const LIGHT_POSITION: f32 = 6.;
     commands
         .spawn(Camera3dBundle {
             transform: Transform::from_xyz(-0.5, -0.5, 15.5).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         })
+        .insert(FlyCam)
         .insert(Name::new("Camera"));
 
-    // Camera Tracking route
-    commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(
-                Mesh::try_from(shape::Icosphere {
-                    radius: 5.125,
+    // * Going through every combination
+    for x in 0..2 {
+        for y in 0..2 {
+            for z in 0..2 {
+                let x = if x % 2 == 0 { 1. } else { -1. };
+                let y = if y % 2 == 0 { 1. } else { -1. };
+                let z = if z % 2 == 0 { 1. } else { -1. };
+                commands.spawn(PointLightBundle {
+                    transform: Transform::from_translation(Vec3::new(x, y, z) * LIGHT_POSITION),
                     ..Default::default()
-                })
-                .unwrap(),
-            ),
-            visibility: Visibility::Hidden,
-            computed_visibility: ComputedVisibility::HIDDEN,
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..Default::default()
-        })
-        .insert(Name::new("Camera Route"));
+                });
+            }
+        }
+    }
+}
 
-    // TODO: Check for better Combinations
-    commands.spawn_batch([
-        PointLightBundle {
-            transform: Transform::from_xyz(-7., -7., -7.),
-            ..Default::default()
-        },
-        PointLightBundle {
-            transform: Transform::from_xyz(-7., -7., 7.),
-            ..Default::default()
-        },
-        PointLightBundle {
-            transform: Transform::from_xyz(7., 7., -7.),
-            ..Default::default()
-        },
-        PointLightBundle {
-            transform: Transform::from_xyz(7., -7., -7.),
-            ..Default::default()
-        },
-    ]);
+#[derive(Component)]
+struct Rotator;
+
+#[derive(Resource)]
+struct RotatorSpeed(f32);
+
+impl Default for RotatorSpeed {
+    fn default() -> Self {
+        Self(1.)
+    }
 }
 
 fn setup_rgb_cube(
@@ -122,37 +110,41 @@ fn setup_rgb_cube(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    const I_GRID_COUNT: i32 = (WINDOW_WIDTH / 256.) as i32;
-    const F_GRID_COUNT: f32 = I_GRID_COUNT as f32;
-    /* let _grid_pad = 0.25; */
+    // ~ For absolute madness xD
+    // * const GRID_COUNT: f32 = WINDOW_WIDTH / 128.;
 
-    for ix in -I_GRID_COUNT..I_GRID_COUNT {
-        for iy in -I_GRID_COUNT..I_GRID_COUNT {
-            for iz in -I_GRID_COUNT..I_GRID_COUNT {
-                let ix = ix as f32;
-                let iy = iy as f32;
-                let iz = iz as f32;
+    const GRID_COUNT: f32 = WINDOW_WIDTH / 256.;
+    const I_GRID_COUNT: i32 = GRID_COUNT as i32;
 
-                let x = ix;
-                let y = iy;
-                let z = iz;
+    commands
+        .spawn((PbrBundle::default(), Rotator))
+        .with_children(|parent| {
+            for x in -I_GRID_COUNT..I_GRID_COUNT {
+                for y in -I_GRID_COUNT..I_GRID_COUNT {
+                    for z in -I_GRID_COUNT..I_GRID_COUNT {
+                        let x = x as f32;
+                        let y = y as f32;
+                        let z = z as f32;
 
-                let r = 255. * (ix / F_GRID_COUNT);
-                let g = 255. * (iy / F_GRID_COUNT);
-                let b = 255. * (iz / F_GRID_COUNT);
-                commands.spawn(PbrBundle {
-                    mesh: meshes.add(
-                        Mesh::try_from(shape::Icosphere {
-                            radius: 0.25,
+                        let r = 255. * (x / GRID_COUNT);
+                        let g = 255. * (y / GRID_COUNT);
+                        let b = 255. * (z / GRID_COUNT);
+                        parent.spawn(PbrBundle {
+                            mesh: meshes.add(
+                                Mesh::try_from(shape::Icosphere {
+                                    radius: 0.25,
+                                    ..Default::default()
+                                })
+                                .unwrap(),
+                            ),
+                            visibility: Visibility::Visible,
+                            material: materials
+                                .add(Color::rgb_u8(r as u8, g as u8, b as u8).into()),
+                            transform: Transform::from_xyz(x, y, z),
                             ..Default::default()
-                        })
-                        .unwrap(),
-                    ),
-                    material: materials.add(Color::rgb_u8(r as u8, g as u8, b as u8).into()),
-                    transform: Transform::from_xyz(x, y, z),
-                    ..Default::default()
-                });
+                        });
+                    }
+                }
             }
-        }
-    }
+        });
 }
